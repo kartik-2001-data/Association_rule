0a4d6ca4e78d244f190f7384305b78f35ac89797
kartik kamat <kartikkamat29@gmail.com>
2023-09-23 16:00:36 +0530

    this is a commit for heroku file

--- app.py
+++ app.py
@@ -1,40 +1,58 @@
-from flask import Flask, render_template, request, jsonify
+import streamlit as st
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 
-app = Flask(__name__)
+# Load the dataset
+@st.cache
+def load_data():
+    df = pd.read_csv('GroceryStoreDataSet.csv', names=['products'], sep=',')
+    return df
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Main function
+def main():
+    st.title('Apriori Association Rule Analysis')
 
-@app.route('/mine_rules', methods=['POST'])
-def mine_rules():
-    try:
-        # Get the CSV file path from the HTML form
-        csv_file_path = request.form['csv_file_path']
-        
-        # Read the CSV data
-        df = pd.read_csv(csv_file_path, names=['products'], sep=',')
-        data = list(df["products"].apply(lambda x: x.split(",")))
+    # Load the data
+    df = load_data()
 
-        # Perform Apriori analysis
-        a = TransactionEncoder()
-        a_data = a.fit(data).transform(data)
-        df = pd.DataFrame(a_data, columns=a.columns_)
-        df = df.replace(False, 0)
-        df = apriori(df, min_support=0.2, use_colnames=True, verbose=1)
-        df_ar = association_rules(df, metric="confidence", min_threshold=0.6)
+    # Show the first few rows of the dataset
+    st.subheader('Dataset')
+    st.dataframe(df.head())
 
-        # Convert the result to a JSON response
-        result = df_ar.to_dict(orient='records')
-        
-        return jsonify({"result": result})
-    
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    # Perform Apriori analysis
+    st.subheader('Apriori Analysis')
+
+    # Define minimum support and confidence values
+    min_support = st.slider('Minimum Support', 0.0, 1.0, 0.2)
+    min_confidence = st.slider('Minimum Confidence', 0.0, 1.0, 0.6)
+
+    # Perform one-hot encoding
+    te = TransactionEncoder()
+    te_ary = te.fit(df['products'].apply(lambda x: x.split(','))).transform(df['products'].apply(lambda x: x.split(',')))
+    df_encoded = pd.DataFrame(te_ary, columns=te.columns_)
+
+    # Run Apriori
+    frequent_itemsets = apriori(df_encoded, min_support=min_support, use_colnames=True)
+    st.write(frequent_itemsets)
+
+    # Association rules
+    st.subheader('Association Rules')
+    rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=min_confidence)
+    st.write(rules)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
 
